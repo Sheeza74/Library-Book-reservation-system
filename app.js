40351fead1ca42949949a5bf95373d42a07761f1
Sheeza Akbar <sheezaakbarr@gmail.com>
2026-05-24 18:36:54 +0500

    updated code

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-function showPage(pageid) {
-    const allpages = ['home','register','login','catalog','dashboard','admin'];
+function showPage(pageId) {
+    const allpages = ['home','register','login','catalog','dashboard',];
     allpages.forEach(function(id) {
         document.getElementById(id).style.display = 'none';
     })
+    document.getElementById(pageId).style.display = 'block';
+
+    if (pageId === 'catalog')   loadBooks();
+    if (pageId === 'dashboard') loadDashboard();
+   
 }
+  
+function showNav(role) {
+    document.getElementById('nav-guest').style.display = 'none';
+    document.getElementById('nav-student').style.display = 'none';
+
+    if(role === 'student') {
+        document.getElementById('nav-student').style.display = 'flex';
+    } else {
+        document.getElementById('nav-guest').style.display = 'flex';
+    }
+
+}
+
